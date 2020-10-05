d5930f91226b0d7d12e5166c49f3a76079bc8c16
Miguel Zambrano <miguelz@email.sc.edu>
2020-10-05 12:06:18 -0400

    deploy1.3

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 5000;
+const request = require("request");
+const start_url = "https://pokeapi.co/api/v2/pokemon/?offset=";
+const end_url = "&limit=20";
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client/build")));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
+
   app.get("*", (req, res) => {
     res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
+
+//build mode
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
-const express = require("express");
-const path = require("path");
-
-const request = require("request");
-const start_url = "https://pokeapi.co/api/v2/pokemon/?offset=";
-const end_url = "&limit=20";
-const app = express();
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "client/build")));
 
 app.post("/api/pokemon/", async (req, res) => {
   let offset = req.body.page;
@@ -98,6 +103,4 @@ function doRequest(url) {
   });
 }
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server running on port ${port}`)
-);
+app.listen(port, () => console.log(`Server running on port ${port}`));
